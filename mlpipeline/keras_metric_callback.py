91d22950dd7eec1a960620c8cd79bab5b00c7005
Otto Zastrow <otto.zastrow@hotmail.de>
2022-06-14 16:26:36 +0200

    incrase log eval

--- mlpipeline/keras_metric_callback.py
+++ mlpipeline/keras_metric_callback.py
@@ -94,7 +94,7 @@ class KerasMetricCallback(Callback):
             # Wrap a tf.data.Dataset around it
             eval_dataset = tf.data.Dataset.from_tensor_slices(eval_dataset).batch(batch_size, drop_remainder=False)
         self.eval_dataset = eval_dataset
-        self.log_interval = 5 if self.args.debug else 500
+        self.log_interval = 5 if self.args.debug else 1000
         self.predict_with_generate = predict_with_generate
         self.output_cols = output_cols
         # This next block attempts to parse out which elements of the dataset should be appended to the labels list
