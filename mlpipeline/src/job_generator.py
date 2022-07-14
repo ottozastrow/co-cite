27cbcb4c418d1ed1136ccf3efb684f463c80dff5
Otto Zastrow <otto.zastrow@hotmail.de>
2022-07-14 17:02:38 +0200

    fixes typo

--- mlpipeline/src/job_generator.py
+++ mlpipeline/src/job_generator.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 with open(args.config_yaml, "r") as f:
     config = yaml.safe_load(f)
 
-all_different_args = config["different"]
+all_different_args = config["different"].copy()
 all_different_args.update(config["different_flags"])
 
 keys = list(all_different_args.keys())
@@ -25,9 +25,10 @@ else:
 for i in range(num_processes):
     # all processes args
     bsub_string = f'bsub -W {config["minutes"]} -n 1 -R "rusage[mem={config["memory"]},ngpus_excl_p=1]" python train.py'
+    
     for item in config["different"].items():
         bsub_string += f" --{item[0]}={item[1][i]}"
-    
+
     # per process flags
     if len(config["different_flags"]) > 0:
         for item in config["different_flags"].items():
@@ -40,8 +41,8 @@ for i in range(num_processes):
             bsub_string += f" --{item[0]}={item[1]}"
     # all processes flags
     if len(config["same_flags"]) > 0:
-        for item in config["same_flags"]:
-            bsub_string += f" --{item}"
+        for flag in config["same_flags"]:
+            bsub_string += f" --{flag}"
     print(bsub_string)
     print()
 
