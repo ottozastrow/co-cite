c490a3161abdc42dd89111087f406d490490417f
Otto Zastrow <otto.zastrow@hotmail.de>
2022-07-14 15:56:32 +0200

    updates democonfig

--- mlpipeline/src/job_generator.py
+++ mlpipeline/src/job_generator.py
@@ -20,18 +20,24 @@ num_processes = len(all_different_args[keys[0]])
 
 # resolve arguments per process
 for i in range(num_processes):
+    # all processes args
     bsub_string = f'bsub -W {config["minutes"]} -n 1 -R "rusage[mem={config["memory"]},ngpus_excl_p=1]" python train.py'
     for item in config["different"].items():
         bsub_string += f" --{item[0]}={item[1][i]}"
-    for item in config["different_flags"].items():
-        bsub_string += f" --{item[0]}={item[1][i]}"
+    
+    # per process flags
+    if len(config["different_flags"]) > 0:
+        for item in config["different_flags"].items():
+            bsub_string += f" --{item[0]}={item[1][i]}"
+    
+    # same keyword args
     for item in config["same"].items():
         if item[1]:
             bsub_string += f" --{item[0]}={item[1]}"
-    # same flags
-    for item in config["same_flags"].items():
-        if item[1]:
-            bsub_string += f" --{item[0]}={item[1]}"
+    # all processes flags
+    if len(config["same_flags"]) > 0:
+        for item in config["same_flags"]:
+            bsub_string += f" --{item[0]}"
     print(bsub_string)
     print()
 
