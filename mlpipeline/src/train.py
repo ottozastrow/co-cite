68e84f237cb4b93fef27bd8f29b95759a947b9da
Otto Zastrow <otto.zastrow@hotmail.de>
2022-07-17 10:05:33 +0200

    eval batchsize to 1

--- mlpipeline/src/train.py
+++ mlpipeline/src/train.py
@@ -63,7 +63,7 @@ def main():
         .shuffle(seed=42)
         .select(list(range(num_demo_samples)))
         .to_tf_dataset(
-            batch_size=args.batchsize,
+            batch_size=1,
             drop_remainder=True,
             columns=["input_ids", "attention_mask", "labels", "label_occurrences"],
             shuffle=False,
@@ -75,7 +75,7 @@ def main():
         .shuffle(seed=42)
         .select(list(range(num_demo_samples)))
         .to_tf_dataset(
-            batch_size=args.batchsize,
+            batch_size=1,
             drop_remainder=True,
             columns=["input_ids", "attention_mask", "labels", "label_occurrences"],
             shuffle=False,
