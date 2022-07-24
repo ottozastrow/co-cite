6fa487a7429610f6da36ed80c0c278e73a603632
Otto vonZastrowMarcks <ovonzastro@eu-login-27.euler.ethz.ch>
2022-07-25 00:08:41 +0200

    removes unnecessary assertions

--- mlpipeline/src/citation_normalization.py
+++ mlpipeline/src/citation_normalization.py
@@ -77,8 +77,8 @@ def sections_from_statute(
                 section = section[:-1]
             new_sections.append(section)
         sections = new_sections
-    if len(statute_orig) > 13:  # in debug mode we may use cropped labels
-        assert len(sections) > 0, "no sections found in statute"
+    if len(sections == 0):
+        print("warning: no sections found in statute", sections, statute_orig, statute)
     # for section in sections:
         # if len(section) == 0:
         #     pass
--- mlpipeline/src/retrieval/run_retrieval.sh
+++ mlpipeline/src/retrieval/run_retrieval.sh
@@ -2,4 +2,4 @@ cd elasticsearch-7.9.2
 ./bin/elasticsearch &
 sleep 40
 cd ../
-python retrieval/retrieve.py --retriever="dpr" --batchsize=4 --epochs=5 --retriever_saved_models=../../model_save/retrieval/data_len_all/dpr/
+python retrieval/retrieve.py --retriever="dpr" --batchsize=4 --epochs=5 --retriever_saved_models=../../model_save/retrieval/data_len_all/dpr/ --notraining
