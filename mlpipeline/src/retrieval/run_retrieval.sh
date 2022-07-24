795a6bf2dad35cdb296afb564bf9ac5de33ab57e
Otto vonZastrowMarcks <ovonzastro@eu-login-05.euler.ethz.ch>
2022-07-24 10:32:11 +0200

    epochs=5

--- mlpipeline/src/retrieval/run_retrieval.sh
+++ mlpipeline/src/retrieval/run_retrieval.sh
@@ -2,4 +2,4 @@ cd elasticsearch-7.9.2
 ./bin/elasticsearch &
 sleep 40
 cd ../
-python retrieval/retrieve.py --samples=10000 --retriever="dpr" --rebuild_dataset --batchsize=4
+python retrieval/retrieve.py --samples=10000 --retriever="dpr" --rebuild_dataset --batchsize=4 --epochs=5
